1b553f75f1638b55391bf64771bf623799f7c7a3
raidipesh78 <raidipesh78@gmail.com>
2020-07-14 13:40:54 +0545

    Modal component added

--- example/src/components/ModalPage.tsx
+++ example/src/components/ModalPage.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-uicomp";
 
 const ModalPage = () => {
-  return <div>MODAL PAGE</div>;
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setVisible(true)}>Open Modal</button>
+      <Modal
+        visible={visible}
+        onClose={() => setVisible(false)}
+        dismissOnOutsideClick={false}
+      >
+        Modal Content Goes Here...
+        <div>
+          <button onClick={() => setVisible(false)}>Close Modal</button>
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default ModalPage;
