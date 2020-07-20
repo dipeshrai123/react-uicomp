89ef371405dba3188cf94215758bd030e3069448
raidipesh78 <raidipesh78@gmail.com>
2020-07-20 14:34:46 +0545

    Accept any props fix

---
+++ example/src/components/Animated.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AnimatedBlock } from "react-uicomp";
+
+const Animated = () => {
+  return (
+    <AnimatedBlock
+      style={{
+        width: 50,
+        height: 50,
+        background: "red",
+      }}
+    >
+      ANIMATED
+    </AnimatedBlock>
+  );
+};
+
+export default Animated;
--- src/animated/Modules.tsx
+++ src/animated/Modules.tsx
@@ -3,6 +3,7 @@ import { animated } from "react-spring";
 
 interface AnimatedBlockProps {
   children?: React.ReactNode;
+  [name: string]: any; // ACCEPT ANY PROPS
 }
 
 // Animated Block - can receive all props from useAnimatedValue() hook
