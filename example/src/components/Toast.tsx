82c52e87cf54b49926a4f7d441205ca6a75b4ff0
raidipesh78 <raidipesh78@gmail.com>
2020-07-30 16:26:34 +0545

    :tada: Toast bug fix and props added

--- example/src/components/Toast.tsx
+++ example/src/components/Toast.tsx
@@ -16,7 +16,7 @@ const ToastPage = () => {
         Open Toast
       </button>
 
-      <Toast {...handler} />
+      <Toast {...handler} timeout={30000} />
     </div>
   );
 };
--- src/ui/Toast.tsx
+++ src/ui/Toast.tsx
@@ -3,11 +3,14 @@ import { useTransition, animated } from "react-spring";
 import styled from "styled-components";
 import { colors, fonts } from "./Constants";
 
+type MessageType = "success" | "error";
+
 const ToastContainer = styled.div`
   position: fixed;
   overflow: hidden;
   display: flex;
   flex-direction: column;
+  z-index: 100;
   right: 40px;
   bottom: 10px;
 `;
@@ -22,9 +25,17 @@ const MessageContainer = styled.div`
   position: relative;
 `;
 
-const Message = styled.div<{ type: "success" | "error" }>`
+interface MessageProps {
+  type: MessageType;
+  successColor?: string;
+  errorColor?: string;
+}
+
+const Message = styled.div<MessageProps>`
   background: ${(props) =>
-    props.type === "success" ? "#68A362" : colors.light.highlightColor};
+    props.type === "success"
+      ? props.successColor || "#68A362"
+      : props.errorColor || colors.light.highlightColor};
   padding: 6px 10px;
   border-radius: 4px;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.12);
@@ -32,21 +43,44 @@ const Message = styled.div<{ type: "success" | "error" }>`
   color: #ffffff;
   font-family: ${fonts.family.arial};
   cursor: pointer;
+  position: relative;
+`;
+
+const IconContainer = styled.div`
+  position: absolute;
+  right: 4px;
+  top: 4px;
 `;
 
 const MasterContainerAnimated = animated(MasterContainer);
 
-type ToastObject = { message: string; type: "error" | "success" };
-type ItemObject = { key: number; message: string; type: "error" | "success" };
+type ToastObject = { message: string; type: MessageType };
+type ItemObject = { key: number; message: string; type: MessageType };
 
 interface ToastProps {
   child: (arg: (toastObj: ToastObject) => void) => void;
-  timeout: number;
+  timeout?: number;
   style?: React.CSSProperties;
+  containerStyle?: Pick<React.CSSProperties, "right" | "bottom">;
+  successColor?: string;
+  errorColor?: string;
+  closeIconColor?: string;
+  closeIconVisible?: boolean;
+  dismissOnClick?: boolean;
 }
 
 let id = 0;
-export const Toast = ({ child, timeout = 4000, style }: ToastProps) => {
+export const Toast = ({
+  child,
+  timeout = 4000,
+  style,
+  containerStyle,
+  successColor,
+  errorColor,
+  closeIconColor = "#ffffff",
+  closeIconVisible = true,
+  dismissOnClick = false,
+}: ToastProps) => {
   const [items, setItems] = useState<Array<ItemObject>>([]);
   const [refMap] = useState(new WeakMap());
 
@@ -62,7 +96,7 @@ export const Toast = ({ child, timeout = 4000, style }: ToastProps) => {
         opacity: 1,
         height: refMap.get(item).offsetHeight,
         onRest: () => {
-          setTimeout(() => onRest(item), timeout); // DELAY SOME TIME
+          setTimeout(() => onRest(item), timeout);
         },
       });
     },
@@ -82,14 +116,35 @@ export const Toast = ({ child, timeout = 4000, style }: ToastProps) => {
   }, [child]);
 
   return (
-    <ToastContainer>
+    <ToastContainer style={containerStyle}>
       {transitions(
         (props, item) =>
           item && (
-            <MasterContainerAnimated onClick={() => onRest(item)} style={props}>
+            <MasterContainerAnimated
+              onClick={dismissOnClick ? () => onRest(item) : () => false}
+              style={props}
+            >
               <MessageContainer ref={(elem) => elem && refMap.set(item, elem)}>
-                <Message {...{ style }} type={item.type}>
+                <Message
+                  {...{ style, successColor, errorColor }}
+                  type={item.type}
+                >
                   {item.message}
+
+                  {closeIconVisible && (
+                    <IconContainer onClick={() => onRest(item)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill={closeIconColor}
+                        width="18px"
+                        height="18px"
+                      >
+                        <path d="M0 0h24v24H0z" fill="none" />
+                        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+                      </svg>
+                    </IconContainer>
+                  )}
                 </Message>
               </MessageContainer>
             </MasterContainerAnimated>
@@ -99,17 +154,12 @@ export const Toast = ({ child, timeout = 4000, style }: ToastProps) => {
   );
 };
 
-type ConfigProps = {
-  timeout: number;
-};
-
-export const useToast = (config?: ConfigProps) => {
+export const useToast = () => {
   const ref = useRef<any>();
 
   return {
     handler: {
       child: (fn: (toastObj: ToastObject) => void) => (ref.current = fn),
-      ...config,
     },
     toast: (toastObj: ToastObject) => ref.current(toastObj),
   };
