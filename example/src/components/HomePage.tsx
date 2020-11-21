17e8dc50297791ca4c33a48f28efde7a5c941556
raidipesh78 <raidipesh78@gmail.com>
2020-11-21 08:20:10 +0545

    ScrollableBlock new Animation Node added for scrolling animation

--- example/src/components/HomePage.tsx
+++ example/src/components/HomePage.tsx
@@ -1,5 +1,39 @@
 import React from "react";
+import { AnimatedBlock, ScrollableBlock, bInterpolate } from "react-uicomp";
+
+const Cards = ({ bg, index }: { bg: string; index: number }) => {
+  const outputRange =
+    index % 2
+      ? ["scale(0.5) translateX(100px)", "scale(1) translateX(0px)"]
+      : ["scale(0.5) translateX(-100px)", "scale(1) translateX(0px)"];
+
+  return (
+    <ScrollableBlock>
+      {(animation: { value: any }) => {
+        return (
+          <AnimatedBlock
+            style={{
+              height: 500,
+              background: bg,
+              transform: bInterpolate(animation.value, outputRange),
+            }}
+          />
+        );
+      }}
+    </ScrollableBlock>
+  );
+};
+
+const colors = ["#39F", "#00ff00", "#FF38EE"];
 
 export default function HomePage() {
-  return <div>HOME PAGE</div>;
+  return (
+    <div style={{ background: "#e1e1e1" }}>
+      {Array(50)
+        .fill(null)
+        .map((_, i) => (
+          <Cards key={i} bg={colors[i % colors.length]} index={i} />
+        ))}
+    </div>
+  );
 }
--- src/animated/Modules.tsx
+++ src/animated/Modules.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { useAnimatedValue } from "./Animation";
 import { animated } from "react-spring";
 
+// Animated Block - can receive all props from useAnimatedValue() hook
 interface AnimatedBlockProps {
   children?: React.ReactNode;
   [name: string]: any; // ACCEPT ANY PROPS
 }
 
-// Animated Block - can receive all props from useAnimatedValue() hook
 export const AnimatedBlock = React.forwardRef(
   (
     { children, ...rest }: AnimatedBlockProps,
@@ -17,3 +18,42 @@ export const AnimatedBlock = React.forwardRef(
     </animated.div>
   ),
 );
+
+// ScrollableBlock - New AnimatedBLock
+// TODO: Bidirectional Animation
+// Container element
+interface ScrollableBlockProps {
+  children?: (animation: any) => React.ReactNode;
+}
+
+export const ScrollableBlock: React.FC<ScrollableBlockProps> = (props) => {
+  const { children } = props;
+  const scrollableBlockRef = React.useRef(null);
+  const animation = useAnimatedValue(0); // 0: not intersecting | 1: intersecting
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      function ([entry]) {
+        const { isIntersecting } = entry;
+
+        if (isIntersecting) {
+          animation.value = 1;
+        } else {
+          console.log(entry);
+          animation.value = 0;
+        }
+      },
+      {
+        root: null, // FOR VIEWPORT ONLY
+        rootMargin: "0px",
+        threshold: [0, 0.8],
+      },
+    );
+
+    observer.observe(scrollableBlockRef.current);
+
+    return () => observer.unobserve(scrollableBlockRef.current);
+  }, []);
+
+  return <div ref={scrollableBlockRef}>{children(animation)}</div>;
+};
