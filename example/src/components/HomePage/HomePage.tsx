237946129b1120b61e0fe217b66ddc26d3f2908a
dipeshrai123 <raidipesh78@gmail.com>
2020-12-02 19:02:25 +0545

    Code refactor Interpolation

--- example/src/components/HomePage/HomePage.tsx
+++ example/src/components/HomePage/HomePage.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { interpolate } from "react-uicomp";
 
 const Homepage = () => {
+  const x = 10;
+
+  const modX = interpolate(x, [0, 100], ["red", "blue"]);
+
+  console.log(modX);
+
   return <div>HOMEPAGE</div>;
 };
 
