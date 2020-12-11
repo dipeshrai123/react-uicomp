d8e3000c15d38213408c6351050a45ef5c7b2a44
dipeshrai123 <raidipesh78@gmail.com>
2020-12-11 15:51:38 +0545

    Examples added

--- example/src/components/ScrollPage/Scroll.tsx
+++ example/src/components/ScrollPage/Scroll.tsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { useScroll } from "react-uicomp";
+import {
+  useScroll,
+  useAnimatedValue,
+  AnimatedBlock,
+  interpolate,
+} from "react-uicomp";
 
 const ScrollPage = () => {
+  const velocityY = useAnimatedValue(0);
+
   useScroll((event) => {
-    console.log(event);
+    velocityY.value = event.velocityY;
   });
+
   return (
     <div style={{ padding: 50 }}>
-      <div style={{ height: 1000, backgroundColor: "#3399ff" }} />
+      {Array(10)
+        .fill(null)
+        .map((_, index) => (
+          <AnimatedBlock
+            key={index}
+            style={{
+              height: 600,
+              marginBottom: 50,
+              backgroundColor: "#3399ff",
+              transform: interpolate(
+                velocityY.value,
+                [-1, 0, 1],
+                ["skewY(-5deg)", "skewY(0deg)", "skewY(5deg)"],
+              ),
+            }}
+          />
+        ))}
     </div>
   );
 };
