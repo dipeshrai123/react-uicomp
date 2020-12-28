80c94de233405e74d97fa22e27790360880cf6e1
dipeshrai123 <raidipesh78@gmail.com>
2020-12-28 21:11:34 +0545

    rubber clamping -> new feature

--- src/animated/Math.ts
+++ src/animated/Math.ts
@@ -3,6 +3,11 @@ export const bin = (booleanValue: boolean) => {
   return booleanValue ? 1 : 0;
 };
 
+// Linear Interpolation
+export const mix = (perc: number, val1: number, val2: number) => {
+  return val1 * (1 - perc) + val2 * perc;
+};
+
 // For clamping values
 export const clamp = (
   value: number,
@@ -12,7 +17,42 @@ export const clamp = (
   return Math.max(lowerbound, Math.min(value, upperbound));
 };
 
-// Linear Interpolation
-export const mix = (perc: number, val1: number, val2: number) => {
-  return val1 * (1 - perc) + val2 * perc;
-};
+function rubber2(distanceFromEdge: number, constant: number) {
+  return Math.pow(distanceFromEdge, constant * 5);
+}
+
+function rubber(distanceFromEdge: number, dimension: number, constant: number) {
+  if (dimension === 0 || Math.abs(dimension) === Infinity) {
+    rubber2(distanceFromEdge, constant);
+  }
+  return (
+    ((distanceFromEdge * dimension * constant) / distanceFromEdge) * constant +
+    dimension
+  );
+}
+
+// Rubber clamping
+export function rubberClamp(
+  value: number,
+  lowerbound: number,
+  upperbound: number,
+  constant: number = 0.15,
+) {
+  if (constant === 0) return clamp(value, lowerbound, upperbound);
+
+  if (value < lowerbound) {
+    return (
+      -rubber(lowerbound - value, upperbound - lowerbound, constant) +
+      lowerbound
+    );
+  }
+
+  if (value > upperbound) {
+    return (
+      +rubber(value - upperbound, upperbound - lowerbound, constant) +
+      upperbound
+    );
+  }
+
+  return value;
+}
