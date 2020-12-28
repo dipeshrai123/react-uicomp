0110ce651c435b2441a8bb90dda80f9aa1908123
dipeshrai123 <raidipesh78@gmail.com>
2020-12-28 22:10:41 +0545

    Example switch complete

--- example/src/components/HomePage/HomePage.tsx
+++ example/src/components/HomePage/HomePage.tsx
@@ -1,19 +1,62 @@
 import * as React from "react";
-import { useDrag, AnimatedBlock } from "react-uicomp";
+import {
+  useDrag,
+  AnimatedBlock,
+  useAnimatedValue,
+  clamp,
+  interpolate,
+} from "react-uicomp";
 
 export default function Homepage() {
-  const bind = useDrag((obj) => {
-    console.log(obj);
+  const CONTAINER_WIDTH = 60;
+  const ELEM_SIZE = CONTAINER_WIDTH / 2;
+  const movementX = React.useRef(0);
+  const offsetX = React.useRef(0);
+  const x = useAnimatedValue(0);
+
+  const bind = useDrag(({ down, movementX: mx }) => {
+    if (down) {
+      movementX.current = clamp(mx + offsetX.current, 0, ELEM_SIZE);
+    } else {
+      movementX.current = movementX.current > ELEM_SIZE / 2 ? ELEM_SIZE : 0;
+      offsetX.current = movementX.current;
+    }
+
+    x.value = movementX.current;
   });
 
   return (
     <AnimatedBlock
-      {...bind()}
       style={{
-        width: 100,
-        height: 100,
-        background: "#3399ff",
+        width: CONTAINER_WIDTH,
+        height: ELEM_SIZE,
+        borderRadius: ELEM_SIZE,
+        border: interpolate(
+          x.value,
+          [0, ELEM_SIZE],
+          ["2px solid #e1e1e1", "2px solid #34c76c"],
+        ),
+        backgroundColor: interpolate(
+          x.value,
+          [0, ELEM_SIZE],
+          ["white", "#34c76cc"],
+        ),
       }}
-    />
+    >
+      <AnimatedBlock
+        {...bind()}
+        style={{
+          position: "relative",
+          top: -1,
+          width: ELEM_SIZE,
+          height: ELEM_SIZE,
+          background: "white",
+          border: "1px solid #e1e1e1",
+          boxShadow: "0px 2px 4px rgba(0,0,0,0.2)",
+          borderRadius: ELEM_SIZE,
+          x: x.value,
+        }}
+      />
+    </AnimatedBlock>
   );
 }
