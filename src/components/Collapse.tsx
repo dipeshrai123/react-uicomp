d5b5dc3f61d4e08058f00c76c023ab92cfd658ab
Ashchal Shrestha <ashchal93@gmail.com>
2021-01-09 16:50:57 +0545

    collapse

---
+++ src/components/Collapse.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+import {
+  AnimatedBlock,
+  useMeasure,
+  interpolate,
+  useAnimatedValue,
+} from "react-ui-animate";
+
+interface CollapseProps {
+  children?: React.ReactNode;
+  expand: boolean;
+  timeout?: number;
+}
+export function Collapse({ children, expand, timeout = 300 }: CollapseProps) {
+  const [divheight, setDivheight] = React.useState<any>(0);
+  const dimension = useMeasure(({ height }) => {
+    setDivheight(height);
+  });
+
+  const open = useAnimatedValue(expand, {
+    duration: timeout,
+  });
+
+  return (
+    <div>
+      <AnimatedBlock
+        style={{
+          height: interpolate(open.value, [0, 1], [0, divheight]),
+          overflow: "hidden",
+        }}
+      >
+        <div style={{ overflow: "hidden" }} {...dimension()}>
+          {children}
+        </div>
+      </AnimatedBlock>
+    </div>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./Dropdown";
 export * from "./DropdownMenu";
 export * from "./Modal";
 export * from "./Toast";
+export * from "./Collapse";
