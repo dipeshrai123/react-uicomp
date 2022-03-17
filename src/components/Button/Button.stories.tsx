bc913490039ba5e45984ecc2dc098655113e4c11
dipeshrai123 <raidipesh78@gmail.com>
2022-03-17 13:10:31 +0545

    alpha version init

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import * as Icon from "react-icons/all";
 
 import { Button } from "./Button";
 
@@ -12,4 +13,5 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   title: "Primary",
+  leftIcon: <Icon.MdFileDownload />,
 };
